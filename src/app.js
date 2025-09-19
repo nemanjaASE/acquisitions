b37ca3de144015bf257e378d77563f1bf65c442f
nemanjaASE <93867316+nemanjaASE@users.noreply.github.com>
2025-09-19 15:02:24 +0200

    Add authentication

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
+import authRouthes from '#routes/auth.routes.js';
+
 const app = express();
 
 app.use(helmet());
@@ -19,5 +21,19 @@ app.get('/', (req, res) => {
   logger.info('Hello from Acquisition!');
   res.status(200).send('Hello from Acquisition!');
 });
+
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'OK',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  });
+});
+
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'Acquisitions API is running' });
+});
+
+app.use('/api/auth', authRouthes);
 
 export default app;
